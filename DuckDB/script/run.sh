5ebc144785615d2c504cc9a5d3b3f60f0c681bd5
Kihwan Kim <luceinaltis2020@naver.com>
2024-12-02 05:39:35 +0000

    init

--- DuckDB/script/run.sh
+++ DuckDB/script/run.sh
@@ -5,7 +5,15 @@ cd ${DIR}
 
 # Default
 BASE_DIR="${DIR}/../duckdb"
+BUILD=RELEASE
+#BUILD=DEBUG
 
 LD_PRELOAD=$(gcc -print-file-name=libasan.so)
 
-${BASE_DIR}/build/debug/duckdb test.duckdb
+# Compile the DuckDB
+if [ ${BUILD} == "DEBUG" ]
+then
+	${BASE_DIR}/build/debug/duckdb db.duckdb
+else
+	${BASE_DIR}/build/release/duckdb db.duckdb
+fi
