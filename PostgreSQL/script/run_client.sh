07d46d38f09bdcab3ea081d7c0e3f9695e580661
Kihwan Kim <luceinaltis2020@naver.com>
2024-12-02 05:57:24 +0000

    init

---
+++ PostgreSQL/script/run_client.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Change to this-file-exist-path.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
+cd $DIR
+
+# Default
+BASE_DIR="$DIR""../../PostgreSQL/"
+USER="ubuntu"
+DATABASE="postgres"
+PORT="5432"
+
+# Parse parameters.
+for i in "$@"
+do
+case $i in
+    -b=*|--base-dir=*)
+    BASE_DIR="${i#*=}"
+    shift
+    ;;
+
+    -u=*|--user=*)
+    USER="${i#*=}"
+    shift
+    ;;
+
+    -p=*|--port=*)
+    PORT="${i#*=}"
+    shift
+    ;;
+
+    -d=*|--database=*)
+    DATABASE="${i#*=}"
+    shift
+    ;;
+
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+
+TARGET_DIR=$BASE_DIR"pgsql/"
+BIN_DIR=$TARGET_DIR"bin/"
+
+LD_LIBRARY_PATH=$TARGET_DIR"lib"
+export LD_LIBRARY_PATH
+
+# run client
+"$BIN_DIR""psql" -p "$PORT" -d "$DATABASE" -U "$USER"
